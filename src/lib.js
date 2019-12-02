a33b63c1e9685816a900bcde03fa2aecd8ca9f17
Satheesh C <satheesancchandran@gmail.com>
2019-12-02 12:02:05 +0530

    runAppTests and app test conditions are added

--- src/lib.js
+++ src/lib.js
@@ -5,7 +5,6 @@ const sum = require("./branches.js").sum;
 const isArgNotValid = require("./validation.js").isArgNotValid;
 const uploadTransaction = require("./utility").uploadTransaction;
 const invalidMessage = require("./utility").invalidMessage;
-const availableJuices = require("./utility").availableJuices;
 const getObjectList = require("./utility").getObjectList;
 const formatter = require("./utility").formatter;
 
@@ -27,9 +26,10 @@ const beverage = function(commandArg, transaction, now, path) {
   let content = save(purchaseDetails, transaction);
   const savedDetails = [purchaseDetails].map(formatter);
   uploadTransaction(path, JSON.stringify(content));
-  savedDetails.unshift(["Employee ID,Beverage,Quantity,Date"]);
-  savedDetails.unshift(["Transaction Recorded:"]);
-
+  savedDetails.unshift(
+    ["Transaction Recorded:"],
+    ["Employee ID,Beverage,Quantity,Date"]
+  );
   return savedDetails;
 };
 
